6a9c909adeca6fb9aa00fc57139c99e7567fe959
Ravi62026 <rs1806105@gmail.com>
2024-08-04 11:44:07 +0530

    llama3.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="DiamondPricePrediction",
+    version="0.0.1",
+    author="Ravi Shankar",
+    author_email="rs1806105@gail.com",
+    description="Diamond Price Prediction using Machine Learning",
+    packages=find_packages(),
+)
