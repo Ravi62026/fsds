70a2b0cf9f1b2dfcf7d844f3f837bfc7de54b4b6
Ravi62026 <rs1806105@gmail.com>
2024-08-06 09:09:12 +0530

    ipynb updated

--- src/DiamondPricePrediction/exception.py
+++ src/DiamondPricePrediction/exception.py
@@ -8,7 +8,7 @@ class customException(Exception):
         self.lineno = exc_tb.tb_lineno
         self.file_name = exc_tb.tb_frame.f_code.co_filename
         
-    def __str__(self):
+    def __str__(self):   # string representation of object
         return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name, self.lineno,str(self.error_message))
         
         
